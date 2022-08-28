29b9f49e693a814dc03456f1921659667ed42990
hayeonchoime <7chlgkdus7@naver.com>
2022-08-28 16:19:46 +0900

    Events Review

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
-const calculator = {
-    add : function(a, b){
-        console.log(a+b);
-    }
-  , minus : function(a,b){
-        console.log(a-b);
-  }
-  , nanu : function(a,b){
-        console.log(a/b);
-  }
-  , powerOf : function(a,b){
-        console.log(a**b); // 거듭제곱
-  }
+const title = document.querySelector("div.hello:first-child h1");
+
+function hadleTitleClick(){
+	console.log("title was clicked!");
 }
 
-calculator.add(3,2);
-calculator.minus(3,2);
-calculator.nanu(3,2);
-calculator.powerOf(3,2);
+title.addEventListener("click", hadleTitleClick);
 
-// #2.13 [나이 확인하기]
-const age = parseInt(prompt("How old are you?"));
-console.log(age);
-//#2.14 ~ #2.15
-console.log(isNaN(age));
-if(isNaN(age) || age < 0){ // not a number : number type 아닐 경우
-      console.log("Please write a real positive number");
-} else if(age < 18) {
-      console.log("You are too young");
-} else if(age >= 18 && age <= 50){
-      console.log("You can drink");
-} else if(age >= 50 && age <= 80){
-      console.log("You should exercise.");
-} else if(age === 100){
-      console.log("wow you are wise");
-} else if(age > 80){
-      console.log("You can do whatever you wandt.");
+function handleMouseEnter(){
+	title.innerText = "Mouse is here!";
+}
+function handleMouseLeave(){
+	title.innerText = "Mouse is gone";
+}
+// 마우스위로울릴때
+title.addEventListener("mouseenter",handleMouseEnter); 
+// = title.onmouseenter = handleMouseEnter;
+
+// 마우스 벗어날때
+title.addEventListener("mouseleave",handleMouseLeave);
+
+function handleWindowResize(){
+	document.body.style.backgroundColor = "tomato";
+    // document.title => <title>태그 
+    // document.head  => <head> 태그
+}
+function handleWindowCopy(){
+	alert("copier!");
+}
+function handleWindowOffline(){
+	alert("SOS no WiFi");
+}
+function handleWindowOnline(){
+	alert("ALL GOOD");
 }
 
-
-
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy); // ctrl+c
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
