2081d4b4ffb43317fb8f9a3cf5e3d52509da28b9
hayeonchoime <7chlgkdus7@naver.com>
2022-08-28 16:58:32 +0900

    css Review

--- app.js
+++ app.js
@@ -1,40 +1,7 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
-function hadleTitleClick(){
-	console.log("title was clicked!");
-}
-
-title.addEventListener("click", hadleTitleClick);
-
-function handleMouseEnter(){
-	title.innerText = "Mouse is here!";
-}
-function handleMouseLeave(){
-	title.innerText = "Mouse is gone";
-}
-// 마우스위로울릴때
-title.addEventListener("mouseenter",handleMouseEnter); 
-// = title.onmouseenter = handleMouseEnter;
-
-// 마우스 벗어날때
-title.addEventListener("mouseleave",handleMouseLeave);
-
-function handleWindowResize(){
-	document.body.style.backgroundColor = "tomato";
-    // document.title => <title>태그 
-    // document.head  => <head> 태그
-}
-function handleWindowCopy(){
-	alert("copier!");
-}
-function handleWindowOffline(){
-	alert("SOS no WiFi");
-}
-function handleWindowOnline(){
-	alert("ALL GOOD");
+function handleTitleClick(){
+      h1.classList.toggle("clicked");
 }
 
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy); // ctrl+c
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click",handleTitleClick);
