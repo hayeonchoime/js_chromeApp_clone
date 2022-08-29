c60a0e9aed794291691b14f6a7f02f182a0b72cf
hayeonchoime <7chlgkdus7@naver.com>
2022-08-30 00:05:20 +0900

    review 220830 AM 12:05

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const loginForm   = document.querySelector("#login-form"); 
 const loginInput  = document.querySelector("#login-form input");
-const loginBtn    = document.querySelector("#login-form button");
 
-function onLoginBtnClick(){
-      const username = loginInput.value;
-      console.log(username);
+function onLoginSubmit(e){
+      e.preventDefault(); // 어떤 이벤트의 기본 동작 등을 방지하는 것.
+      console.log(e);
+
+      //const username = loginInput.value;
+      //console.log(username);
 }
-loginBtn.addEventListener("click",onLoginBtnClick);
+
+loginForm.addEventListener("submit",onLoginSubmit);
